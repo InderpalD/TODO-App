e80f37dbf435420d0522a4bc27ef17923436c6e7
InderpalD <isdhillo@ucsd.edu>
2020-07-28 20:51:35 -0400

    Added some frontend code for input todo and listtodo

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React, {Fragment} from 'react';
 import './App.css';
 
 // Component imports
-import InputTodo from './components/InputTodo'
+import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodo';
 
 function App() {
   return (
     <Fragment>
       <div className='container'>
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   )
